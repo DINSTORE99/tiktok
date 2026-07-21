f27d9fa4686cc8d43c72bb31f7339ff95a20437c
DINSTORE99 <dinstoree41@gmail.com>
2026-07-21 23:14:20 +0700

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-gray-800 py-6 text-center text-gray-400">
-      © 2026 DIN STORE • All Rights Reserved
-    </footer>
-  );
-}
